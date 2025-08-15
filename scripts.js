f202e6eb1580284305a03e75d10abdd92a1c14d4
work <137403706+AltheaVestrit@users.noreply.github.com>
2025-08-15 16:27:31 +0200

    print temperature per day on website

--- scripts.js
+++ scripts.js
@@ -1,5 +1,22 @@
 // Query builder: https://www.visualcrossing.com/weather-query-builder/
 
+const weekDays = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const p = document.querySelector("p");
+
 async function getWeatherData(location) {
   const API_key = "Z85Y9JDDWDK4FHFWLRN9FY6Y8";
   const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&elements=datetime%2CresolvedAddress%2Ctempmax%2Ctempmin%2Ctemp%2Cprecip%2Cprecipprob%2Cpreciptype%2Cwindspeed%2Cwinddir%2Ccloudcover%2Cuvindex&include=days%2Cfcst&&key=${API_key}&contentType=json`;
@@ -9,12 +26,25 @@ async function getWeatherData(location) {
   return json;
 }
 
-const p = document.querySelector("p");
+function decideWeatherIcon(precip, precipprob, preciptype, cloudcover) {}
 
-getWeatherData("London").then((response) =>
-  console.log(
-    response.days[0].datetime,
-    "||",
-    response.days[0].temp + " degrees Celcius"
-  )
-);
+function decidePrecipitationIcon(precip, precipprob, preciptype) {}
+
+function decideWindIcon(winddir, windspeed) {}
+
+function formatDate(dateInput) {
+  const date = new Date(dateInput);
+  const weekDay = weekDays[date.getDay()];
+  const day = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+  return { weekDay, day, month, year };
+}
+
+getWeatherData("London").then((response) => {
+  for (let i = 0; i < 7; i++) {
+    const date = formatDate(response.days[i].datetime);
+    p.innerText += `${date.weekDay} ${date.day} ${date.month} ${date.year}|${response.days[i].temp} °C\n`;
+    console.log(response);
+  }
+});
